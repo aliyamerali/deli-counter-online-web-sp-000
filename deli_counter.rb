679f5a09db6c3ec430562c08ed5b484440f65e98
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-25 16:33:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 #
 def line(deli)
-  if deli.length == 0
+  if deli.empty?
     puts "The line is currently empty."
   else
     line_list = deli.map{ |customer| "#{deli.index(customer) + 1}. #{customer}"}
